a4ebccc2d783d8d2a1e382a68f250c4f88f2f87d
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-04-02 20:51:09 -0500

    Switch back to vanilla js

---
+++ public/js/index.js
@@ -0,0 +1,17 @@
+{/* <>
+      {head_bar}
+      <HorizontalBorder/>
+      <div className='container-horizontal'>
+        {mini_side_bar}
+        <VerticalBorder/>
+        {side_bar}
+        <VerticalBorder/>
+        <div id='main-content-container'>
+            <div id='tab-container'>
+              {selectedTab && tabs[selectedTab]}
+            </div>
+            <HorizontalBorder/>
+            {tabs_bar}
+        </div>
+      </div>
+    </> */}
